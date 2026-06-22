4cb216f12fe249e04723ac59b605acd3d0d5723a
maitruongan <truongnguyenmanh411@gmail.com>
2026-06-22 21:56:32 +0700

    create exam repository

---
+++ TestManagement.DAL/Repositories/ExamRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestManagement.DAL.Data;
+using TestManagement.DAL.Models;
+using TestManagement.DAL.Repositories.Interfaces;
+
+namespace TestManagement.DAL.Repositories
+{
+    public class ExamRepository : Repository<Exam>, IExamRepository
+    {
+        public ExamRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+        public async Task<Exam?> GetDetailAsync(int id)
+        {
+            return await Context.Exams
+                .Include(x=> x.Subject)
+                .Include(x=> x.Creator)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public IQueryable<Exam> GetQueryable()
+        {
+            return Context.Exams.AsNoTracking()
+                .Include(x => x.Subject)
+                .Include(x => x.Creator)
+                .Where(x=> !x.IsDeleted);
+        }
+    }
+}
---
+++ TestManagement.DAL/Repositories/Interfaces/IExamRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestManagement.DAL.Models;
+
+namespace TestManagement.DAL.Repositories.Interfaces
+{
+    public interface IExamRepository : IRepository<Exam>
+    {
+        Task<Exam?> GetDetailAsync(int id);
+        IQueryable<Exam> GetQueryable();
+    }
+}
